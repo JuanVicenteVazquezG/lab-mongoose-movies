4d40a8d377030b07c2325fe5bbfa30241c5dabe0
Juan Vicente Vazquez G <juanvicentevazquezg@gmail.com>
2019-08-27 07:00:12 +0200

    Done

--- routes/movies.js
+++ routes/movies.js
@@ -7,11 +7,27 @@ const router = express.Router();
 
 const Movie = require('../models/movie.js');
 
+
+router.get('/new', (req, res, next) => {
+  res.render('movies/new');
+});
+
+router.post('/', (req, res, next) => {
+  const { title, genre, plot } = req.body;
+  const newMovie = new Movie({ title, genre, plot });
+  newMovie.save()
+    .then((aMovie) => {
+      res.redirect('/movies');
+    })
+    .catch((error) => {
+      console.log(`error ${error}`);
+      res.render('movies/new');
+    });
+});
+
 router.get('/', (req, res, next) => {
-  console.log('holaaaaaaaaaaa');
   Movie.find({})
     .then((theMovies) => {
-      console.log(theMovies);
       res.render('movies/index', { theMovies });
     })
     .catch((error) => {
@@ -19,7 +35,7 @@ router.get('/', (req, res, next) => {
     });
 });
 
-router.get('/:id', (req, res, next) => { 
+router.get('/:id', (req, res, next) => {
   const { id } = req.params;
   Movie.findById({ _id: id })
     .then((aMovie) => {
@@ -28,6 +44,36 @@ router.get('/:id', (req, res, next) => {
     .catch((error) => {
       next(error);
     });
+});
+
+router.post('/:id/delete', (req, res, next) => {
+  const { id } = req.params;
+  Movie.findByIdAndDelete(id)
+    .then(() => {
+      res.redirect('/movies');
+      console.log('Movie Removed');
+    })
+    .catch((error) => { next(error); });
+});
+
+router.get('/:id/edit', (req, res, next) => {
+  const { id } = req.params;
+  Movie.findOne({ _id: id })
+    .then((aMovie) => {
+      console.log(`Movie finded ${aMovie}`);
+      res.render('movies/edit', aMovie);
+    })
+    .catch((error) => { next(error); });
+});
+
+router.post('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const { title, genre, plot } = req.body;
+  Movie.update({ _id: id }, { $set: { title, genre, plot } })
+    .then((aMovie) => {
+      res.redirect(id);
+    })
+    .catch((error) => { next(error); });
 });
 
 module.exports = router;
